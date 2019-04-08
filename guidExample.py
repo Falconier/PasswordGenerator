113b0739ae5553358e0586148d48a431128e196a
Jacob Emory Bullin <36140031+Falconier@users.noreply.github.com>
2019-04-08 15:38:21 -0400

    April8Afternoon
    - added guidExample.py

---
+++ guidExample.py
@@ -0,0 +1,21 @@
+##region File Info:
+# File Name: guidExample.py
+# globaly unique identifier or GUID's for short
+# https://docs.python.org/2/library/uuid.html
+##endregion
+
+import uuid
+
+guid = uuid.uuid4() ##produces a standard GUID
+
+strGUID = str(guid) ##or str(uuid.uuid4())
+
+hexGUID = uuid.uuid4().hex ##basically just removes the '-'s from the standard
+
+byteGUID = uuid.uuid4().bytes ##no idea why on earth you would ever need this, but this splits the guid into its raw bytes
+
+print(guid)
+print(strGUID)
+print(hexGUID)
+print(byteGUID)
+
